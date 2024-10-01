287f0cf2af1a4f970a21d1efe87fe5f9db6ae993
Maxim <maxim.oweyssi@est.org.uk>
2024-10-01 13:10:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ color_discrete_map = {
 def load_data():
     # Replace with the path to your data file
     df                          = pd.read_csv('GreenComplexity_CZE_2022.csv')
-    url = 'https://docs.google.com/spreadsheets/d/1440MNmn8N_mIPqW6ZrPv4RDG-_kPy7oUYsik4sMF3o8/gviz/tq?tqx=out:csv'
+    url = 'https://docs.google.com/spreadsheets/d/1mhv7sJC5wSqJRXdfyFaWtBuEpX6ENj2c/gviz/tq?tqx=out:csv'
     taxonomy = pd.read_csv(url)
     CZE = pd.read_csv('CZE.csv')
     GreenProducts = taxonomy.merge(CZE,how='left',left_on='HS_ID',right_on='prod')
