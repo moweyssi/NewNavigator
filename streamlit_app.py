2c761984c5913c9a6efb2398ee69fe9c6044f0fc
Maxim <maxim.oweyssi@est.org.uk>
2024-10-16 09:59:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -368,9 +368,10 @@ fig.update_layout(
         x=0.5             # Position it at the center of the graph
     ),
     plot_bgcolor=background_color,
-    paper_bgcolor = background_color
-    
-                
+    paper_bgcolor = background_color          
+)
+fig.update_traces(
+    hovertemplate='%{label}: %{value}<extra></extra>'
 )
 st.plotly_chart(fig)
 col1, col2, col3 = st.columns(3)
